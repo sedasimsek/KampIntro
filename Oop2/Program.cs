a135d780d9ee72fecf6ed4e9d513c799a3bdb0fc
sedasimsek <sedasimsekk27@gmail.com>
2021-01-24 03:08:11 +0300

    23 Ocak Ders Sonu

---
+++ Oop2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Engin Demiroğ
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "Engin";
+            musteri1.Adi = "Demiroğ";
+            musteri1.Soyadi = 1;
+            musteri1.Tcno = "1234567890";
+
+
+            //Kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+
+            //Gerçek Müşteri - Tüzel Müşteri
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+        }
+    }
+}
